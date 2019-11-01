157ee95225f0793afb8c43ad8dedc2b2d183faeb
Koichi Nakanishi <keizo042dev@gmail.com>
2019-11-02 04:35:11 +0900

    fix

--- server/server.go
+++ server/server.go
@@ -153,33 +153,3 @@ func (c *Server) handle(conn *net.TCPConn) error {
 	}
 	return nil
 }
-
-// loadKeys loads a signing key and optionally loads a file containing old keys for redemption validation
-func (c *Server) loadKeys() error {
-	if c.SignKeyFilePath == "" {
-		return ErrEmptyKeyPath
-	}
-	if c.CommFilePath == "" {
-		return ErrEmptyCommPath
-	}
-
-	// Parse current signing key
-	_, currkey, err := crypto.ParseKeyFile(c.SignKeyFilePath, true)
-	if err != nil {
-		return err
-	}
-	c.signKey = currkey[0]
-	c.redeemKeys = append(c.redeemKeys, c.signKey)
-
-	// optionally parse old keys that are valid for redemption
-	if c.RedeemKeysFilePath != "" {
-		errLog.Println("Adding extra keys for verifying token redemptions")
-		_, oldKeys, err := crypto.ParseKeyFile(c.RedeemKeysFilePath, false)
-		if err != nil {
-			return err
-		}
-		c.redeemKeys = append(c.redeemKeys, oldKeys...)
-	}
-
-	return nil
-}
